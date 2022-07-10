3c11f649f20a298c4d9aeeabd0e0c2ad7ec4b280
eyx.ofc@gmail.com <eyx.ofc@gmail.com>
2022-07-10 13:08:28 -0300

    up

--- js/Musica.js
+++ js/Musica.js
@@ -7,13 +7,12 @@ var indexMusica = 0;
 var Musicas = [
   'verde',
   'roses',
-  'kevin'
+  'kevin',
+  'lofi'
 ]
 
 
-
 e.addEventListener('click',()=>{
-  
   if(indexMusica === 0){
     indexMusica = Musicas.length - 1;
     audio.src = `./audio/${Musicas[indexMusica]}.mp3`;
@@ -24,14 +23,11 @@ e.addEventListener('click',()=>{
     audio.src = `./audio/${Musicas[indexMusica]}.mp3`;
     capa.style.backgroundImage = `url(./img/${Musicas[indexMusica]}.jpeg)`;
   }
-  
   Despause()
-  
 })
 
 
 d.addEventListener('click',()=>{
-  
   if(indexMusica === Musicas.length - 1){
     indexMusica = 0;
     audio.src = `./audio/${Musicas[indexMusica]}.mp3`;
@@ -41,29 +37,22 @@ d.addEventListener('click',()=>{
     audio.src = `./audio/${Musicas[indexMusica]}.mp3`;
     capa.style.backgroundImage = `url(./img/${Musicas[indexMusica]}.jpeg)`
   }
-  
   Despause()
-  
 })
 
-start.addEventListener('click',Despause)
 
+start.addEventListener('click',Despause);
 function Despause(){
   start.src = './img/pause.png';
-  
   start.addEventListener('click',Pause);
   start.removeEventListener('click',Despause)
-  
   audio.play()
-  
 }
 
 function Pause(){
   start.src = './img/despause.png';
-  
   start.addEventListener('click',Despause);
   start.removeEventListener('click',Pause)
-  
   audio.pause();
 }
 
