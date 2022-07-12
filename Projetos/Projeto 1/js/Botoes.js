7c283de44e43bdad5258a0d692a7304c34a447b0
eyx.ofc@gmail.com <eyx.ofc@gmail.com>
2022-07-12 12:46:06 -0300

    up

---
+++ Projetos/Projeto 1/js/Botoes.js
@@ -0,0 +1,36 @@
+var Cima = document.querySelector('.topo');
+var Direita = document.querySelector('.direita');
+var Esquerda = document.querySelector('.esquerda');
+var Baixo = document.querySelector('.baixo');
+
+function AdicionarBotoes(){
+  Cima.addEventListener('click',Ymais);
+  Baixo.addEventListener('click',Ymenos);
+  Direita.addEventListener('click',Xmais);
+  Esquerda.addEventListener('click',Xmenos);
+}
+
+function RemoverBotoes(){
+  
+}
+
+
+
+
+
+
+function Xmais(){
+  
+}
+
+function Xmenos(){
+  
+}
+
+function Ymais(){
+  
+}
+
+function Ymenos(){
+  
+}
