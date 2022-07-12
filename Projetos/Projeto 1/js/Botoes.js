eefbdd7e9889a1c10bc0ab2f2ab58fe0632d31ac
eyx.ofc@gmail.com <eyx.ofc@gmail.com>
2022-07-12 20:16:35 -0300

    up

--- Projetos/Projeto 1/js/Botoes.js
+++ Projetos/Projeto 1/js/Botoes.js
@@ -21,21 +21,34 @@ function RemoverBotoes(){
 
 function Xmais(){
   Global.Maca.x += 10;
+  console.log(Global.Maca.x);
+  if (Global.Maca.x === Number('300')) {
+  Global.Maca.x = 0;
+  }
   window.document.querySelector('.Maca').style.left = Global.Maca.x + 'px';
 }
 
 function Xmenos(){
   Global.Maca.x -= 10;
+  if (Global.Maca.x === Number('-20')) {
+  Global.Maca.x = 280;
+  }
   window.document.querySelector('.Maca').style.left = Global.Maca.x + 'px';
 }
 
 function Ymais(){
   Global.Maca.y += 10;
+  if (Global.Maca.y === Number('200')) {
+  Global.Maca.y = 0;
+  }
   window.document.querySelector('.Maca').style.top = Global.Maca.y + 'px';
 }
 
 function Ymenos(){
   Global.Maca.y -= 10;
+  if(Global.Maca.y === Number('-20')){
+  Global.Maca.y =  180;
+  }
   window.document.querySelector('.Maca').style.top = Global.Maca.y + 'px';
 }
 
