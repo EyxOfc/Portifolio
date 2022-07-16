989361a61e99aa6d9b9cf035efe6cde1fbe0b33f
eyx.ofc@gmail.com <eyx.ofc@gmail.com>
2022-07-16 19:59:38 -0300

    up

--- Projetos/Projeto 1/js/Cronometro.js
+++ Projetos/Projeto 1/js/Cronometro.js
@@ -1,12 +1,15 @@
 import Global from './Global.js';
 
 Global.Game.Segundos = 10;
+var Iniciar = document.querySelector('#iniciar');
 
 function Cronometro(){
   if(Global.Game.Segundos === 0){
     Global.Funcoes.GameOff();
+    Iniciar.addEventListener('click',Global.Funcoes.Game);
   }else{
     Global.Game.Segundos -= 1;
+    Iniciar.innerHTML = Global.Game.Segundos;
     setTimeout(Cronometro,1000);
   }
 }
--- Projetos/Projeto 1/js/Game.js
+++ Projetos/Projeto 1/js/Game.js
@@ -1,22 +1,25 @@
 import Global from './Global.js';
 
-
+var Iniciar = document.querySelector('#iniciar');
+Iniciar.addEventListener('click',Game)
 
 function Game(){
   setTimeout(()=>{
     Global.Funcoes.AdicionarBotoes();
     Global.Funcoes.Cronometro();
     Global.Game.Segundos = 10;
-  },0)
+  },0);
+  Iniciar.removeEventListener('click',Game);
 }
 
 function GameOff(){
   setTimeout(()=>{
     Global.Funcoes.RemoverBotoes();
   },0);
+  Iniciar.innerHTML = '';
+  Iniciar.appendChild(document.createElement('img'));
+  Iniciar.childNodes[0].src = './img/play.png'
 }
 
 Global.Funcoes.Game = Game;
 Global.Funcoes.GameOff = GameOff;
-
-Game();
